241763ed9c0fe75dd0562c749b7e680f6790058b
Henrry <henrrydanielmejia@gmail.com>
2021-07-16 22:19:32 -0400

    first commit

---
+++ src/main/java/com/oneteam/socialcoders/SocialcodersApplication.java
@@ -0,0 +1,13 @@
+package com.oneteam.socialcoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialcodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialcodersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneteam/socialcoders/SocialcodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneteam.socialcoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialcodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

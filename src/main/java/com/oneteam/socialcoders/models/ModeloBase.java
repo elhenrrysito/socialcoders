e0ea7bb726052fe513cadea8cf8c513e9311c238
Henrry <hectorroyaleesponja@gmail.com>
2021-07-19 15:37:31 -0400

    Agregué lombok
    agregué lombok a la clase ModeloBase

--- src/main/java/com/oneteam/socialcoders/models/ModeloBase.java
+++ src/main/java/com/oneteam/socialcoders/models/ModeloBase.java
@@ -1,5 +1,13 @@
 package com.oneteam.socialcoders.models;
 
+import javax.persistence.MappedSuperclass;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter @Setter @NoArgsConstructor
 public abstract class ModeloBase {
     
 }
